ee98875d0d3d27a81a5a39c2edd000a4fe68a293
Eren Önal <75484033+erenonal@users.noreply.github.com>
2024-05-14 20:27:34 +0300

    up

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCx71Ne8SjpA2BPKFQSjFoW8Nal8mrhV-Q",
-  authDomain: "simularge-69e6b.firebaseapp.com",
-  projectId: "simularge-69e6b",
-  storageBucket: "simularge-69e6b.appspot.com",
-  messagingSenderId: "48610520884",
-  appId: "1:48610520884:web:63d1a7ee2140c3d4c3bf61",
 };
 
 const app = initializeApp(firebaseConfig);
